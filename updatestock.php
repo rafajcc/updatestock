5f0a8034054270f6897ec3cf56e3b1dbb93dc969
rafajcc <rafajcc@gmail.com>
2026-06-14 18:27:30 +0200

    VT-94 changes for v1.1.1 (add colours to logs)

--- updatestock.php
+++ updatestock.php
@@ -17,7 +17,7 @@ class UpdateStock extends Module
     {
         $this->name = 'updatestock';
         $this->tab = 'administration';
-        $this->version = '1.1.0';
+        $this->version = '1.1.1';
         $this->author = 'Vera Technology';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
